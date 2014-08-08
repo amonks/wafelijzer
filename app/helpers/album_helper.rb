b592f2c730cf36c3811ad9c4083a19ab909fc016
Andrew Monks <a@monks.co>
2014-08-08 08:35:48 -0400

    more bandcamp automation

--- app/helpers/album_helper.rb
+++ app/helpers/album_helper.rb
@@ -1,12 +1,12 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 module Wafelijzer
-  class App
-    module AlbumHelper
+	class App
+		module AlbumHelper
 			def bandcamp_embed_from_id id
 				'<div class="bandcamp"><iframe style="border: 0; width:100%;" src="http://bandcamp.com/EmbeddedPlayer/album=' + id.to_s + '/size=large/bgcol=ffffff/linkcol=000000/transparent=true/" seamless></iframe></div>'
 			end
-    end
-    helpers AlbumHelper
-  end
+		end
+		helpers AlbumHelper
+	end
 end
