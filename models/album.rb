d9530b66eceba908d541af5009371a844c594661
Andrew Monks <a@monks.co>
2014-08-08 08:45:38 -0400

    better handling of title

--- models/album.rb
+++ models/album.rb
@@ -8,7 +8,7 @@ class Album < Sequel::Model
     	response = ::JSON.parse(open("http://api.bandcamp.com/api/url/1/info?key=vatnajokull&url=" + self.bandcamp_url).read)
     	self.this.update(:bandcamp_id => response["album_id"].to_s)
     	response = ::JSON.parse(open("http://api.bandcamp.com/api/album/2/info?key=vatnajokull&album_id=" + response["album_id"].to_s).read)
-        self.this.update(:title => response["name"])
+        self.this.update(:title => response["title"])
         self.this.update(:about => response["about"])
     	self.this.update(:credits => response["credits"])
     	self.this.update(:cover_url => response["large_art_url"])
