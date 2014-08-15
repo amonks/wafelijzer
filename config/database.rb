4dd8a898f6d12fc887a56f4a11b95b1011d0f0f6
Andrew Monks <a@monks.co>
2014-08-15 05:03:59 -0400

    puma

--- config/database.rb
+++ config/database.rb
@@ -3,6 +3,6 @@ Sequel::Model.plugin(:timestamps)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("jdbc:postgresql://localhost/wafelijzer_development", :loggers => [logger])
-  when :production  then Sequel.connect(ENV['DATABASE_URL'],  :loggers => [logger])
+  when :production  then Sequel.connect("jdbc:" + ENV['DATABASE_URL'],  :loggers => [logger])
   when :test        then Sequel.connect("jdbc:postgresql://localhost/wafelijzer_test",        :loggers => [logger])
 end
