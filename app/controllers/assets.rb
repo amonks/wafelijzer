fb6c9336b97095124392eafdc1ebf96ba8e3fe92
Andrew Monks <a@monks.co>
2014-08-19 22:37:48 -0400

    demo for packery issue

--- app/controllers/assets.rb
+++ app/controllers/assets.rb
@@ -2,17 +2,11 @@
 #
 # We'll use this file to handle routing for theme CSS files.
 
-Wafelijzer::App.controllers :artist do
+Wafelijzer::App.controllers :assets do
 
   # This route is for the artists index at `/artists`
   get :css, :cache => true, :map => '/assets/theme.css' do
     content_type 'text/css'
     theme_raw_css(setting_value('theme'))
   end
-
-  # This is the route at `/decor`
-  get :index, :map => '/decor' do
-    render 'blank', :layout => 'layouts/decor'
-  end
-
 end
